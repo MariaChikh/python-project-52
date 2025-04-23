143febd391362fd141b2628713db5de90e8e8f36
Maria Chikhireva <mchikhireva@gmail.com>
2025-04-24 00:16:03 +0300

    fix urls

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
-
 from task_manager.users.forms import CustomUserForm
 
 
